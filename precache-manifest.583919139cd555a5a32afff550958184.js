46921e22fadec886494e5142d6676f69be47670d
Cian Hardiman Ray <71712549+CianRH@users.noreply.github.com>
2022-08-30 14:26:55 +0100

    Updates

---
+++ precache-manifest.583919139cd555a5a32afff550958184.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4546a006a9408df994576728c24882c3",
+    "url": "/portfolio-2/index.html"
+  },
+  {
+    "revision": "5be9689f6a86bf5f2afd",
+    "url": "/portfolio-2/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "fdb7e3ad873631773f42",
+    "url": "/portfolio-2/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "5be9689f6a86bf5f2afd",
+    "url": "/portfolio-2/static/js/2.e26a8252.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio-2/static/js/2.e26a8252.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdb7e3ad873631773f42",
+    "url": "/portfolio-2/static/js/main.32a8a740.chunk.js"
+  },
+  {
+    "revision": "8ee8d87afbf1d748f139",
+    "url": "/portfolio-2/static/js/runtime-main.d66f7b68.js"
+  }
+]);
